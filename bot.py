b23af0b789a2afeb634865c90a2326d8dc020d11
Thg-Projects <thgkamikaz@gmail.com>
2023-02-19 12:22:40 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,35 @@
 import telebot
+import time
+from iqoptionapi.stable_api import IQ_Option
+Iq=IQ_Option("sobralface@gmail.com","facebook123@")
+Iq.connect() #conectar
+goal="EURUSD"
+print("get candles")
+# print(Iq.get_candles(goal,60,111,time.time()))
+
+print("BALANCE Seu Teste:   ")
+print(Iq.get_balance())
+
+
+print("você vai verificar qual moeda você usa:   ")
+print(Iq.get_currency())
 
 CHAVE_API = "5865616008:AAElJsLWJcEALxuhNIZPi0u0jJeqOvJkOHg"
 
 bot = telebot.TeleBot(CHAVE_API)
+
+@bot.message_handler(commands=["banca"])
+def banca(mensagem):
+    myBalance = Iq.get_balance()
+    bot.send_message(mensagem.chat.id, "sua banca com get_balance() é: "+myBalance)
+
+@bot.message_handler(commands=["moeda"])
+def moeda(mensagem):
+    myMoeda = Iq.get_currency()
+    bot.send_message(mensagem.chat.id, "qual moeda você usa com get_currency(): "+myMoeda)
+    
+    
+    
 
 @bot.message_handler(commands=["pizza"])
 def pizza(mensagem):
