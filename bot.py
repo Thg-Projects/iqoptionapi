a73068fdadd81772e2feb7b3d6aafb9411bd66ae
Thg-Projects <thgkamikaz@gmail.com>
2023-02-19 12:34:08 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,37 +17,6 @@ print(Iq.get_currency())
 CHAVE_API = "5865616008:AAElJsLWJcEALxuhNIZPi0u0jJeqOvJkOHg"
 
 bot = telebot.TeleBot(CHAVE_API)
- 
-    
-
-@bot.message_handler(commands=["pizza"])
-def pizza(mensagem):
-    bot.send_message(mensagem.chat.id, "Saindo a pizza pra sua casa: Tempo de espera em 20min")
-
-@bot.message_handler(commands=["hamburguer"])
-def hamburguer(mensagem):
-    bot.send_message(mensagem.chat.id, "Saindo o Brabo: em 10min chega ai")
-
-@bot.message_handler(commands=["salada"])
-def salada(mensagem):
-    bot.send_message(mensagem.chat.id, "Não tem salada não, clique aqui para iniciar: /iniciar")
-
-@bot.message_handler(commands=["opcao1"])
-def opcao1(mensagem):
-    texto = """
-    O que você quer? (Clique em uma opção)
-    /pizza Pizza
-    /hamburguer Hamburguer
-    /salada Salada"""
-    bot.send_message(mensagem.chat.id, texto)
-
-@bot.message_handler(commands=["opcao2"])
-def opcao2(mensagem):
-    bot.send_message(mensagem.chat.id, "Para enviar uma reclamação, mande um e-mail para reclamação@balbalba.com")
-
-@bot.message_handler(commands=["opcao3"])
-def opcao3(mensagem):
-    bot.send_message(mensagem.chat.id, "Valeu! Lira mandou um abraço de volta")
     
 @bot.message_handler(commands=["balance"])
 def balance(mensagem):
@@ -62,7 +31,7 @@ def moeda(mensagem):
 @bot.message_handler(commands=["banca"])
 def banca(mensagem):
     myBalance = Iq.get_balance()
-    bot.send_message(mensagem.chat.id, "sua banca com get_balance() é: "+myBalance)
+    bot.send_message(mensagem.chat.id, "sua banca com get_balance() é: "+str(myBalance))
 
 def verificar(mensagem):
     return True
@@ -71,9 +40,9 @@ def verificar(mensagem):
 def responder(mensagem):
     texto = """
     Escolha uma opção para continuar (Clique no item):
-     /opcao1 Fazer um pedido
-     /opcao2 Reclamar de um pedido
-     /opcao3 Mandar um abraço pro Lira
+     /banca Fazer um pedido
+     /balance Reclamar de um pedido
+     /moeda Mandar um abraço pro Lira
 Responder qualquer outra coisa não vai funcionar, clique em uma das opções"""
     bot.reply_to(mensagem, texto)
 
